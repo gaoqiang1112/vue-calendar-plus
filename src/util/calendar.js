51e672ea80efba0c52f891ed04ceda75e9060cf5
[gaoqiang] <[gaoqiang1112@163.com]>
2018-05-24 16:25:05 +0800

    删除多余代码

--- src/util/calendar.js
+++ src/util/calendar.js
@@ -1,14 +1,4 @@
-/**
- * @1900-2100区间内的公历、农历互转
- * @charset UTF-8
- * @Author  Jea杨(JJonline@JJonline.Cn)
- * @Time    2014-7-21
- * @Time    2016-8-13 Fixed 2033hex、Attribution Annals
- * @Time    2016-9-25 Fixed lunar LeapMonth Param Bug
- * @Version 1.0.2
- * @公历转农历：calendar.solar2lunar(1987,11,01); //[you can ignore params of prefix 0]
- * @农历转公历：calendar.lunar2solar(1987,09,10); //[you can ignore params of prefix 0]
- */
+
 var calendar;
 calendar = {
 
